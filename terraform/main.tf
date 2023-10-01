f095dd0f275abba8d487c0ec8863a06a9369f878
kyuye <ivyheart@gmail.com>
2023-10-01 15:47:49 +0900

    terraform tested

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,13 @@ provider "aws" {
   region = "ap-northeast-2"  
 }
 
-resource "aws_instance" "ai-web-application" {
+resource "aws_instance" "ai_web_application" {
   ami           = "ami-02288bc8778f3166f"  
-  instance_type = "t2.micro"      
+  instance_type = "t2.large"
 
+  key_name = aws_key_pair.cicd_make_keypair.key_name
+  vpc_security_group_ids = [aws_security_group.cicd_sg.id]
+  associate_public_ip_address = true
 
   user_data = file("setup.sh")
 }
