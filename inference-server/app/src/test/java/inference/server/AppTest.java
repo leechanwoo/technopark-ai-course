6f5c268cf19d59d32d1bbfb009222826aa7f42c4
kyuye <ivyheart@gmail.com>
2023-10-24 07:16:57 +0900

    remote testing

--- inference-server/app/src/test/java/inference/server/AppTest.java
+++ inference-server/app/src/test/java/inference/server/AppTest.java
@@ -68,6 +68,34 @@ class AppGrpcTest {
         OrtEnvironment env = OrtEnvironment.getEnvironment();
 
         OrtSession.SessionOptions opts = new SessionOptions();
+        // opts.setOptimizationLevel(OptLevel.BASIC_OPT);
+        // opts.addCUDA(0);
+
+        // OrtSession session = env.createSession(resources_path + "/mobilenetv2-10.onnx", opts);
+        // assertNotEquals(null, session);
+    }
+
+    @Test
+    public void gpuTest3() throws OrtException, IOException {
+        String resources_path = System.getenv("RESOURCES_PATH");
+
+        OrtEnvironment env = OrtEnvironment.getEnvironment();
+
+        OrtSession.SessionOptions opts = new SessionOptions();
+        opts.setOptimizationLevel(OptLevel.BASIC_OPT);
+        // opts.addCUDA(0);
+
+        // OrtSession session = env.createSession(resources_path + "/mobilenetv2-10.onnx", opts);
+        // assertNotEquals(null, session);
+    }
+
+    @Test
+    public void gpuTest4() throws OrtException, IOException {
+        String resources_path = System.getenv("RESOURCES_PATH");
+
+        OrtEnvironment env = OrtEnvironment.getEnvironment();
+
+        OrtSession.SessionOptions opts = new SessionOptions();
         opts.setOptimizationLevel(OptLevel.BASIC_OPT);
         opts.addCUDA(0);
 
@@ -76,7 +104,7 @@ class AppGrpcTest {
     }
 
     @Test
-    public void gpuTest3() throws OrtException, IOException {
+    public void gpuTest5() throws OrtException, IOException {
         String resources_path = System.getenv("RESOURCES_PATH");
 
         OrtEnvironment env = OrtEnvironment.getEnvironment();
